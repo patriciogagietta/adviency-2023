b5b2f3e94895c8bf11fe4b9e2f7b783401440bdd
patriciogagietta1 <patoxxgagietta1@gmail.com>
2023-12-29 12:38:34 -0300

    dia-02

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ function App() {
   return (
     <>
       <div className='h-screen flex flex-col justify-center items-center'>
-          <h1>Regalos: </h1>
+        <div className='bg-slate-300 p-20 rounded flex flex-col gap-3 shadow-2xl'>
+          <h1 className='text-4xl'>Regalos: </h1>
           <ul>
             <li>Botas</li>
             <li>Botines</li>
             <li>Camiseta</li>
           </ul>
+        </div>
       </div>
     </>
   )
